a096545fc53a5186b974bfeecdb73aafbccbd0a8
Jeyabalaji Subramanian <jeyabalaji.subramanian@gmail.com>
2021-10-19 12:36:44 +0530

    Update aurora-test.tf
    removed random sequence and renamed parameter group names.

--- aurora-test.tf
+++ aurora-test.tf
@@ -1,33 +1,28 @@
 locals {
-    name = "dbiusamea-casc-cn-pg-dev"
-    region = "us-east-1"
-    vpc_id = "vpc-c6230da2"
-    database_subnet_group_name = "dbsgusameaprivate"
-    tags = {
+  name                       = "dbiusamea-casc-cn-pg-dev"
+  region                     = "us-east-1"
+  vpc_id                     = "vpc-c6230da2"
+  database_subnet_group_name = "dbsgusameaprivate"
+  tags = {
     ITOwnerEmail    = "ravi_kiranj@cargill.com"
-    ApplicationName = "dbiusamea-casc-cn-aurora-postgres-dev"
-    Environment = "dev"
-    Terraform = "true"
-    }
+    ApplicationName = "casc-cn-aurora-postgres-dev"
+    Environment     = "dev"
+    Terraform       = "true"
+  }
 }
 
 ################################################################################
 # Supporting Resources
 ################################################################################
 
-resource "random_integer" "sequence" {
-  min     = 2000
-  max     = 9999
-}
-
-resource "aws_db_parameter_group" "example" {
+resource "aws_db_parameter_group" "db_param_group" {
   name_prefix = "${local.name}-parameter-group"
   family      = "aurora-postgresql11"
   description = "${local.name}-parameter-group"
   tags        = local.tags
 }
 
-resource "aws_rds_cluster_parameter_group" "example" {
+resource "aws_rds_cluster_parameter_group" "db_cluster_param_group" {
   name_prefix = "${local.name}-cluster-parameter-group"
   family      = "aurora-postgresql11"
   description = "${local.name}-cluster-parameter-group"
@@ -42,13 +37,13 @@ module "rds-aurora" {
   source  = "terraform-aws-modules/rds-aurora/aws"
   version = "5.2.0"
 
-  name                            = "${local.name}${random_integer.sequence.result}"
+  name                            = "${local.name}"
   engine                          = "aurora-postgresql"
-  engine_version                  = "11.9"  
+  engine_version                  = "11.9"
   instance_type                   = "db.r4.large"
   instance_type_replica           = "db.r4.large"
-  db_cluster_parameter_group_name = aws_db_parameter_group.example.id
-  db_parameter_group_name         = aws_rds_cluster_parameter_group.example.id
+  db_cluster_parameter_group_name = aws_db_parameter_group.db_param_group.id
+  db_parameter_group_name         = aws_rds_cluster_parameter_group.db_cluster_param_group.id
   enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
 
   replica_count         = 1
@@ -56,20 +51,20 @@ module "rds-aurora" {
   replica_scale_min     = 1
   replica_scale_max     = 5
 
-  monitoring_interval           = 60
-  iam_roles                     = ["arn:aws:iam::496395248431:role/rds-monitoring-role"]
+  monitoring_interval = 60
+  iam_roles           = ["arn:aws:iam::496395248431:role/rds-monitoring-role"]
 
   apply_immediately   = true
   skip_final_snapshot = true
 
-  allowed_security_groups         = ["sg-09f82175e1ae28431"]
-  storage_encrypted               = true
+  allowed_security_groups = ["sg-09f82175e1ae28431"]
+  storage_encrypted       = true
 
-  vpc_id                = "${local.vpc_id}"
-  db_subnet_group_name  = "${local.database_subnet_group_name}"
+  vpc_id                = local.vpc_id
+  db_subnet_group_name  = local.database_subnet_group_name
   create_security_group = true
 }
-    
+
 # aws_rds_cluster
 output "rds_cluster_id" {
   description = "The ID of the cluster"
